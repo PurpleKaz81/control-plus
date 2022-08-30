ba8d1fe04e532a5984b6d253ec894f5e066719d2
Gabriel Teixeira Soares <gabriel_soares10@hotmail.com>
2022-08-30 19:09:20 -0300

    termino da pagina home

--- app/controllers/sessions_controllers.rb
+++ app/controllers/sessions_controllers.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::SessionsController
   # def after_sign_in_path_for(_resource)
-  #   root_path
+  #   dashboard_path
   # end
 
   # def after_sign_out_path_for(_resource)
