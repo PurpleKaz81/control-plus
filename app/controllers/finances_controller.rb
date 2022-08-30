67b88f789357a98cd8a02e4ecd42931dd954bf1a
Rafael Kasinski <rakasin@gmail.com>
2022-08-29 21:40:25 -0300

    Finished finances controller and views. All models, controllers, and views, as well as routes, are done

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,2 +1,41 @@
 class FinancesController < ApplicationController
+  def index
+    @finances = Finance.all
+  end
+
+  def new
+    @finance = Finance.new
+  end
+
+  def create
+    @finance = Finance.new(strong_params)
+    @finance.user = current_user
+    if @finance.save
+      redirect_to finances_path, notice: 'Nova entrada/saída criada com sucesso.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @finance = Finance.find(params[:id])
+  end
+
+  def update
+    @finance = Finance.find(params[:id])
+    @finance.update(strong_params)
+    redirect_to finances_path, notice: 'Entrada/Saída editada com sucess.'
+  end
+
+  def destroy
+    @finance = Finance.find(params[:id])
+    @finance.destroy
+    redirect_to finances_path, notice: 'Sua Entrada/Saída Foi Deletada COm Sucesso.'
+  end
+
+  private
+
+  def strong_params
+    params.require(:finance).permit(:category, :description, :value, :date)
+  end
 end
--- app/models/finance.rb
+++ app/models/finance.rb
@@ -2,5 +2,5 @@ class Finance < ApplicationRecord
   belongs_to :user
   validates :category, :description, :value, :date, presence: true
   # validates :category
-  validates :description, numericality: { minumum: 5 }
+  validates :description, length: { minimum: 5 }
 end
