7ff2f2f8d45fc3f26a871cc9041289a836133da2
Gabriel Teixeira Soares <gabriel_soares10@hotmail.com>
2022-08-30 12:01:33 -0300

    navbar2

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_202126) do
+ActiveRecord::Schema[7.0].define(version: 20_220_829_202_126) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "finances", force: :cascade do |t|
-    t.string "category"
-    t.text "description"
-    t.decimal "value"
-    t.date "date"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_finances_on_user_id"
+  create_table 'finances', force: :cascade do |t|
+    t.string 'category'
+    t.text 'description'
+    t.decimal 'value'
+    t.date 'date'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_finances_on_user_id'
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.text "content"
-    t.date "date"
-    t.integer "order"
-    t.boolean "completed"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
+  create_table 'tasks', force: :cascade do |t|
+    t.text 'content'
+    t.date 'date'
+    t.integer 'order'
+    t.boolean 'completed'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_tasks_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'phone_number'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "finances", "users"
-  add_foreign_key "tasks", "users"
+  add_foreign_key 'finances', 'users'
+  add_foreign_key 'tasks', 'users'
 end
