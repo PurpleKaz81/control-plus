09e01e1dacf3aa2d1f4f4d7ed6d6ccb87c24e489
Rafael Kasinski <rakasin@gmail.com>
2022-08-31 14:19:04 -0300

    Worked on edit page and form partial and worked on inflow/outflow views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+User.destroy_all
+
+puts 'Generating user'
+
+user = User.new(first_name: 'teste', last_name: 'teste', email: 'teste@teste.com', password: '123123',
+                phone_number: '11999999999')
+
+user.save
+
+puts 'Generating three inflows'
+
+inflow1 = Finance.new(
+  date: '22/01/2022',
+  description: 'Casa de repousa da minha mãe',
+  value: rand(1..99),
+  user:,
+  category: 'inflow'
+)
+inflow1.save!
+
+inflow2 = Finance.new(
+  date: '04/03/2022',
+  description: 'Arroucho do Partidão',
+  value: rand(500..999),
+  user:,
+  category: 'inflow'
+)
+inflow2.save!
+
+inflow3 = Finance.new(
+  date: '05/04/2022',
+  description: 'Subonro por cigarro, cela 23B',
+  value: rand(1..75),
+  user:,
+  category: 'inflow'
+)
+inflow3.save!
+
+puts 'Finished!'
