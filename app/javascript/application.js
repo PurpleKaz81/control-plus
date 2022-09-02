2fa4b7a5b6f47865e7580f624f19b022c51fb27b
Rafael Kasinski <rakasin@gmail.com>
2022-09-02 12:02:49 -0300

    Fixed total value to blink red when negative

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,27 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// const color = document.getElementById("total-color");
+// let total;
+// if (color < 10) {
+//   total = "Good morning";
+// } else if (color < 20) {
+//   total = "Good day";
+// } else {
+//   total = "Good evening";
+// }
+
+const total = document.getElementById("total-color").classList.add("red");
+console.log("HI");
+// total.classList.add("red");
+
+
+
+
+
+// if (total > 0) {
+//   total = element.classList.add(".green");
+// } else {
+//   total = element.classList.add(".red");
+// };
