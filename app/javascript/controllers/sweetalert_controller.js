3030986480ab422d1eb7509b9cf82bf4dd2c7269
Rafael Kasinski <rakasin@gmail.com>
2022-09-08 15:45:35 -0300

    Fixed trash button in tasks and footer fixed

--- app/javascript/controllers/sweetalert_controller.js
+++ app/javascript/controllers/sweetalert_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="sweetalert"
 export default class extends Controller {
   connect() {
-    console.log('passei aqui');
     this.redirect = false;
   }
 
