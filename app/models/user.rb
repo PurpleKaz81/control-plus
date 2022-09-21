2665201ee5923c4599a98afa3178b78bc83d4976
Rafael Kasinski <rakasin@gmail.com>
2022-09-21 13:50:22 -0300

    Fixed password regexp validations, still missing final message in pt-BR

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tasks, dependent: :destroy
   has_many :finances, dependent: :destroy
-  validates :first_name, :last_name, :phone_number, :email, :password, presence: true
-  # validates :email, format: { with: /\S+@\S+\.\S+/, message: 'Use um email válido' }
+  validates :first_name, :last_name, :phone_number, :email, presence: true
+  PASSWORD_REQUIREMENTS = /\A
+    (?={.6})
+    (?=.*\d)
+    (?=.*[a-z])
+    (?=.*[A-Z])
+    (?=.*[[:^alnum:]])
+  /x
+  validates :password, presence: true, format: PASSWORD_REQUIREMENTS
   has_one_attached :photo
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -179,6 +179,7 @@ Devise.setup do |config|
   # ==> Configuration for :validatable
   # Range for password length.
   config.password_length = 6..128
+  # config.password_regexp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
