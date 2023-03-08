71158c412e2569cba3655a8e4c8f21ab69b6eddf
Fabian Carlos <fabian.pow@gmail.com>
2023-03-08 16:38:30 -0400

    navbar fix, erase account fix, javascript fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def destroy
     @user = current_user
     @user.destroy
-    redirect_to root_url(confirm: 'delete') if @user.destroy
+    # redirect_to root_url(confirm: 'delete') if @user.destroy
+    redirect_back(fallback_location: root_url(confirm: 'delete'), allow_other_host: false) if @user.destroy
   end
 end
--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,18 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const currentlyActive = document.querySelector(".active")
-    const elementsDiv = document.querySelector("#navbar-elements")
+    const currentlyActive = document.querySelector(".active");
+    const elementsDiv = document.querySelector("#navbar-elements");
     const list = document.querySelectorAll('.list');
+    const indicator = document.getElementById('indicator');
+    const correct_pos = window.innerWidth < 600 ? 35 : 30;
+    let last_target = null;
+
+    const focusIndicator = () => {
+      if (currentlyActive) {
+        let left_pos = currentlyActive.offsetLeft;
+
+        indicator.style.transform = `translateX(calc(${left_pos - correct_pos}px))`;
+      }
+    }
+    focusIndicator();
+
     elementsDiv.addEventListener('mouseout', () => {
-      list.forEach((item) =>
-        item.classList.remove('active'));
-      currentlyActive.classList.add('active');
+      list.forEach((item) => {
+        item.classList.remove('active')
+      });
+
+      if (currentlyActive) {
+        currentlyActive.classList.add('active');
+        focusIndicator();
+      } else {
+        // FAQ, ABOUT, RECURSOS
+        last_target.classList.add('active');
+      }
     });
 
     const activeLink = (event) => {
-      list.forEach((item) =>
-      item.classList.remove('active'));
+      let left_pos = event.currentTarget.offsetLeft;
+      last_target = event.currentTarget;
+
+      indicator.style.transform = `translateX(calc(${left_pos - correct_pos}px))`;
+
+      list.forEach((item) => item.classList.remove('active'));
       event.currentTarget.classList.add('active');
     }
     list.forEach((item) =>
